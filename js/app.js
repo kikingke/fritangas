85a8c7bbf5b97a4aa2572059ba5a9886c37e2799
Enrique <eabucam@gmail.com>
2018-08-22 15:29:49 -0400

    gg

--- js/app.js
+++ js/app.js
@@ -15,9 +15,25 @@ var firebase = app_firebase;
   function logOut(){
     firebase.auth().signOut();
   }
-  function fnCreate(){
-    firebase.auth().signOut();
+  
+  function messageHandler(err){
+    if(!!err){
+      console.log(err);
+    }else{
+       console.log("Success");
+    }
   }
+  
+  function fnCreate(){
+    var path = 'users/'+ uid;
+    var data = {
+      name:"Gabriel",
+      age:29,
+      messsage:"Hello"
+    }
+    app_fireBase.databaseApi.create(path,data,messageHandler);
+  }
+  
   function fnRead(){
     firebase.auth().signOut();
   }
