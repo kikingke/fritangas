b5ca9b804d509f0cfc436b7161c28a84b5d96839
Enrique <eabucam@gmail.com>
2018-08-22 15:06:59 -0400

    t

--- js/app.js
+++ js/app.js
@@ -1,2 +1,38 @@
 
+var mainApp = {};
+(function(){
+var firebase = app_firebase;
+  var uid = null;
+  firebase.auth().onAuthStateChanged(function(user){
+    if(user){
+      uid = user.uid;
+    }else{
+      uid = null;
+      window.location.replace("login.html");
+    }
+  });
+  
+  function logOut(){
+    firebase.auth().signOut();
+  }
+  function fnCreate(){
+    firebase.auth().signOut();
+  }
+  function fnRead(){
+    firebase.auth().signOut();
+  }
+  function fnUpdate(){
+    firebase.auth().signOut();
+  }
+  function fnDelete(){
+    firebase.auth().signOut();
+  }
+  
+  mainApp.Create = fnCreate;
+  mainApp.Read = fnRead;
+  mainApp.Update = fnUpdate;
+  mainApp.Delete = fnDelete;
+  mainApp.logOut = logout;
+  
 
+})
