4f02a84eac3f9f05762690ecd0e8bf17e4646f06
Enrique <eabucam@gmail.com>
2018-08-22 15:00:29 -0400

    j

---
+++ js/firebases.js
@@ -0,0 +1,15 @@
+var app_firebase = {};
+(function(){
+var config = {
+    apiKey: "AIzaSyCZmaSdOSRPIuCRNhyPCgS5LoQHO-SlBOk",
+    authDomain: "fritangas-c82e6.firebaseapp.com",
+    databaseURL: "https://fritangas-c82e6.firebaseio.com",
+    projectId: "fritangas-c82e6",
+    storageBucket: "fritangas-c82e6.appspot.com",
+    messagingSenderId: "964180946238"
+  };
+  firebase.initializeApp(config);
+  
+  app_firebase = firebase;
+
+})
