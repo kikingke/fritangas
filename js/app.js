ff3ad39dbb9a708ff37fca6875b85a9ada2b9300
Enrique <eabucam@gmail.com>
2018-08-28 11:46:40 -0400

    gh

--- js/app.js
+++ js/app.js
@@ -1,56 +1,55 @@
+$(document).ready(function(){
 
-// var mainApp = {};
-// (function(){
-// var firebase = app_fireBase;
-//   var uid = null;
-//   firebase.auth().onAuthStateChanged(function(user){
-//     if(user){
-//       uid = user.uid;
-//        console.log(uid);
-//     }else{
-//       console.log("redirect");
-//       uid = null;
-//       window.location.replace("login.html");
-//     }
-//   });
-  
-//   function logOut(){
-//     firebase.auth().signOut();
-//   }
-  
-//   function messageHandler(err){
-//     if(!!err){
-//       console.log(err);
-//     }else{
-//        console.log("Success");
-//     }
-//   }
-  
-//   function fnCreate(){
-//     var path = 'users/'+ uid;
-//     var data = {
-//       name:"Gabriel",
-//       age:29,
-//       messsage:"Hello"
-//     }
-//     app_fireBase.databaseApi.create(path,data,messageHandler);
-//   }
-  
-//   function fnRead(){
-//     firebase.auth().signOut();
-//   }
-//   function fnUpdate(){
-//     firebase.auth().signOut();
-//   }
-//   function fnDelete(){
-//     firebase.auth().signOut();
-//   }
-  
-//   mainApp.Create = fnCreate;
-//   mainApp.Read = fnRead;
-//   mainApp.Update = fnUpdate;
-//   mainApp.Delete = fnDelete;
-//   mainApp.logOut = logout;
-  
+ load_json_data('country');
 
-// })
+ function load_json_data(id, parent_id)
+ {
+  var html_code = '';
+  $.getJSON('mundep.json', function(data){
+
+   html_code += '<option value="">Select '+id+'</option>';
+   $.each(data, function(key, value){
+    if(id == 'country')
+    {
+     if(value.parent_id == '0')
+     {
+      html_code += '<option value="'+value.id+'">'+value.name+'</option>';
+     }
+    }
+    else
+    {
+     if(value.parent_id == parent_id)
+     {
+      html_code += '<option value="'+value.id+'">'+value.name+'</option>';
+     }
+    }
+   });
+   $('#'+id).html(html_code);
+  });
+
+ }
+
+ $(document).on('change', '#country', function(){
+  var country_id = $(this).val();
+  if(country_id != '')
+  {
+   load_json_data('state', country_id);
+  }
+  else
+  {
+   $('#state').html('<option value="">Select state</option>');
+   $('#city').html('<option value="">Select city</option>');
+  }
+ });
+ $(document).on('change', '#state', function(){
+  var state_id = $(this).val();
+  if(state_id != '')
+  {
+   load_json_data('city', state_id);
+  }
+  else
+  {
+   $('#city').html('<option value="">Select city</option>');
+  }
+ });
+});
